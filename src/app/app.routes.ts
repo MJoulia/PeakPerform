cced0462f17b287e75b04901911ac7d56cf84381
MJoulia <mazid.joulia@gmail.com>
2024-12-14 01:02:00 +0100

    login update create account finish add projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CreateaccountComponent } from './createaccount/createaccount.component';
+import { ProjetComponent } from './projet/projet.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },   // Default route
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent},
-  { path: 'createaccount', component: CreateaccountComponent}
+  { path: 'createaccount', component: CreateaccountComponent},
+  { path: 'projet', component: ProjetComponent }
 ];
 
 @NgModule({
